cf5aa01d66d4101f7606dbe4ec891873399fcec5
melnikovpetr123 <36701059+melnikovpetr123@users.noreply.github.com>
2018-09-23 14:30:53 +0000

    Update app-instances.tf

--- app-instances.tf
+++ app-instances.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   access_key  = "${var.access_key}"
   secret_key  = "${var.secret_key}"
-  region      = "${var.region}"
+/*  region      = "${var.region}" */
 }
 resource "aws_instance" "master" {
   ami           = "ami-00035f41c82244dab"
